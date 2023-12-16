e77bd1a46634e1f45d430a29e4a0f159639804a3
Walter Kaunda <walterkaunda@gmail.com>
2023-12-16 09:39:56 +0200

    Improve day 15 solutions

--- 2023/15/part-1.scala
+++ 2023/15/part-1.scala
@@ -1,37 +1,27 @@
 import scala.io.StdIn
 import scala.collection.mutable.ListBuffer
-import scala.io.BufferedSource
-import scala.collection.mutable.Buffer
 
 package Day15 {
   object Part1 {
     @main def main: Unit =
-      val value =
-        readInput
-          .map(computeHash)
-          .reduce(_ + _)
+      println(readInput.map(computeHash).reduce(_ + _))
 
-      println { value }
-
-    def readInput: Iterable[String] =
-      var line = StdIn.readLine()
+    def readInput: Iterable[String] = {
       val inputsBuf = ListBuffer[String]()
+      var line = ""
 
-      while line != null do
+      while { line = StdIn.readLine(); line != null } do {
         line = line.trim()
         inputsBuf.addAll(line.split(","))
-        line = StdIn.readLine()
+      }
 
       inputsBuf.toList
+    }
 
     def computeHash(step: String): Int =
-      var accum = 0
-
-      for char <- step.chars().toArray() do
-        accum += char.toInt
-        accum *= 17
-        accum %= 256
-
-      accum
+      step
+        .chars()
+        .toArray()
+        .foldLeft(0)((accum, char) => ((accum + char.toInt) * 17) % 256)
   }
 }
--- 2023/15/part-2.scala
+++ 2023/15/part-2.scala
@@ -1,49 +1,44 @@
 import scala.io.StdIn
 import scala.collection.mutable.ListBuffer
-import java.io.EOFException
-import scala.io.Source
-import java.io.IOException
 import scala.collection.mutable.ArrayDeque
 
 package Day15 {
   object Part2 {
-    @main def main: Unit =
+    @main def main: Unit = {
+      val totalFocusingPower =
+        getBoxes.zipWithIndex
+          .map((box, i) => boxFocusingPower(box, i))
+          .sum
+
+      println(totalFocusingPower)
+    }
+
+    def getBoxes: Array[Array[(String, Int)]] = {
       val boxes = (0 to 256).map(_ => ArrayDeque[(String, Int)]()).toArray
 
-      for (label, symbol, focalLength) <- readInput do
+      for (label, symbol, focalLength) <- readInput do {
         val hash = computeHash(label)
+        val index = boxes(hash).find(_._1 == label).map(boxes(hash).indexOf(_))
 
-        if symbol == '=' then
-          val index = boxes(hash)
-            .find(_._1 == label)
-            .map(boxes(hash).indexOf(_))
+        index match {
+          case Some(i) if symbol == '=' => {
+            boxes(hash).insert(i, (label, focalLength.get))
+            boxes(hash).remove(i + 1, 1)
+          }
+          case Some(i) if symbol == '-' => {
+            boxes(hash).remove(i, 1)
+          }
+          case None if symbol == '=' => {
+            boxes(hash).addOne((label, focalLength.get))
+          }
+          case _ => {}
+        }
+      }
 
-          if index.isEmpty then boxes(hash).addOne((label, focalLength.get))
-          else
-            boxes(hash).insert(index.get, (label, focalLength.get))
-            boxes(hash).remove(index.get + 1, 1)
-        else if symbol == '-' then
-          val index = boxes(hash)
-            .find(_._1 == label)
-            .map(boxes(hash).indexOf(_))
+      boxes.map(_.toArray)
+    }
 
-          if !index.isEmpty then boxes(hash).remove(index.get, 1)
-
-      val focalLengths =
-        boxes
-          .zip((0 to boxes.length))
-          .map((box, i) => {
-            box
-              .zip(0 to box.length)
-              .foldLeft(0)((accum, box_item) => {
-                val (lens, j) = box_item
-                accum + (i + 1) * ((j + 1) * lens._2)
-              })
-          })
-
-      println(focalLengths.sum)
-
-    def readInput: Iterable[(String, Char, Option[Int])] =
+    def readInput: Iterable[(String, Char, Option[Int])] = {
       val buff = ListBuffer[(String, Char, Option[Int])]()
       var label = ""
       var symbol = 0.toChar
@@ -51,35 +46,42 @@ package Day15 {
 
       var line = ""
 
-      while { line = StdIn.readLine; line != null } do
-        for ordinal <- line.chars().toArray() do
+      while { line = StdIn.readLine; line != null } do {
+        for ordinal <- line.chars().toArray() do {
           val char = ordinal.toChar
 
-          char match
-            case ',' =>
+          char match {
+            case ',' => {
               buff.addOne((label, symbol, focalLength))
               label = ""
               symbol = '\u0000'
               focalLength = None
-            case '=' | '-' =>
+            }
+            case '=' | '-' => {
               symbol = char
-            case _ if char.isDigit =>
+            }
+            case _ if char.isDigit => {
               if focalLength.isEmpty then focalLength = Some(char.asDigit)
               else focalLength = focalLength.map(_ * 10 + char.asDigit)
-            case _ =>
+            }
+            case _ => {
               label += char
+            }
+          }
+        }
+      }
 
       buff.addOne((label, symbol, focalLength))
       buff.toList
+    }
 
     def computeHash(step: String): Int =
-      var accum = 0
+      step.chars.toArray
+        .foldLeft(0)((accum, char) => ((accum + char.toInt) * 17) % 256)
 
-      for char <- step.chars().toArray() do
-        accum += char.toInt
-        accum *= 17
-        accum %= 256
-
-      accum
+    def boxFocusingPower(box: Iterable[(String, Int)], boxNumber: Int): Int =
+      box.zipWithIndex
+        .map((lens, i) => (boxNumber + 1) * (i + 1) * lens._2)
+        .foldLeft(0)((accum, focusingPower) => accum + focusingPower)
   }
 }
